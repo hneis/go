0d785e965d177af99ebd829b1e88b0524cf5e6ce
ivanyuta.dmitriy@gmail.com <ivanyuta.dmitriy@gmail.com>
2019-10-22 20:08:59 +0500

    3. Найти сумму вклада через 5 лет

--- lesson1/main.go
+++ lesson1/main.go
@@ -2,25 +2,16 @@ package main
 
 import (
 	"fmt"
-
-	"../lesson1/geometry"
-)
-
-const (
-	DOLLAR_EXCHANGE = 65
 )
 
 func main() {
-	fmt.Println("Площадь, периметр и гипотенуза прямоугольного треугольника")
+	fmt.Println("Рассчитать сумму вклада через 5 лет")
 
-	var a, b float64
+	var deposit, rate float64
 
-	fmt.Printf("Введите сторону a: ")
-	fmt.Scan(&a)
-	fmt.Printf("Введите сторону b: ")
-	fmt.Scan(&b)
-	c := geometry.TriangleHypotenuse(a, b)
-	fmt.Printf("Площадь S=%0.3f\n", geometry.TriangleArea(a, b))
-	fmt.Printf("Периметр P=%0.3f\n", geometry.TrianglePerimeter(a, b, c))
-	fmt.Printf("Гипотенуза c=%0.3f\n", c)
+	fmt.Printf("Сумма вклада: ")
+	fmt.Scan(&deposit)
+	fmt.Printf("Годовой процент: ")
+	fmt.Scan(&rate)
+	fmt.Printf("Сумма вклада через пять лет составит: %0.3f\n", deposit+(deposit*rate*5))
 }
